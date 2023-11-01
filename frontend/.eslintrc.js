3c84ff0309d568d2db93a510bd04a4574c8cdaa8
Moises weber <weber_moises@sicredi.com.br>
2023-11-01 12:58:28 -0300

    iniciando o projeto

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:vue/vue3-recommended",
+        "@vue/eslint-config-typescript/recommended",
+    ],
+    rules: {
+        indent: ["error", 4],
+        semi: ["error", "always"],
+        quotes: ["error", "double"],
+        "vue/multi-word-component-names": "off",
+        "comma-spacing": ["error", { before: false, after: true }],
+        "@typescript-eslint/ban-ts-comment": 0,
+    },
+};
